ef3b43c43d9c2672d1ab1ea5de81a1bb126bd101
rajeshkumar1130 <74873530+rajeshkumar1130@users.noreply.github.com>
2021-07-10 20:46:55 +0530

    add exit menu to tray icon, font change

---
+++ Speed.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace speedMeter
+{
+    partial class Speed
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Upload = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Download = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.UploadText = new System.Windows.Forms.Label();
+            this.DownloadText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Upload
+            // 
+            this.Upload.Text = "notifyIcon1";
+            this.Upload.Visible = true;
+            // 
+            // Download
+            // 
+            this.Download.Text = "notifyIcon1";
+            this.Download.Visible = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // UploadText
+            // 
+            this.UploadText.AutoSize = true;
+            this.UploadText.ForeColor = System.Drawing.Color.Red;
+            this.UploadText.Location = new System.Drawing.Point(124, 166);
+            this.UploadText.Name = "UploadText";
+            this.UploadText.Size = new System.Drawing.Size(51, 20);
+            this.UploadText.TabIndex = 0;
+            this.UploadText.Text = "label1";
+            // 
+            // DownloadText
+            // 
+            this.DownloadText.AutoSize = true;
+            this.DownloadText.ForeColor = System.Drawing.Color.Green;
+            this.DownloadText.Location = new System.Drawing.Point(124, 268);
+            this.DownloadText.Name = "DownloadText";
+            this.DownloadText.Size = new System.Drawing.Size(51, 20);
+            this.DownloadText.TabIndex = 1;
+            this.DownloadText.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 459);
+            this.Controls.Add(this.DownloadText);
+            this.Controls.Add(this.UploadText);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon Upload;
+        private System.Windows.Forms.NotifyIcon Download;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label UploadText;
+        private System.Windows.Forms.Label DownloadText;
+    }
+}
+
---
+++ Speed.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using Echevil;
+using System.Windows.Forms;
+
+namespace speedMeter
+{
+    public partial class Speed : Form
+    {
+        static NetworkMonitor networkMonitor = new NetworkMonitor();
+        static NetworkAdapter[] networkAdapters = networkMonitor.Adapters;
+        public Speed()
+        {
+            InitializeComponent();
+            TrayMenuContext();
+        }
+
+        private string getDownloadSpeed()
+        {
+            string text = "-1";
+            foreach (var adapter in networkAdapters)
+            {
+                text = $"{adapter.DownloadSpeed / Math.Pow(1024, 2):N1}";
+                UploadText.Text = $"DL: {text}";
+            }
+            return text;
+        }
+        private string getUploadSpeed()
+        {
+            string text = "-1";
+            foreach (var adapter in networkAdapters)
+            {
+                text = $"{adapter.UploadSpeed / Math.Pow(1024, 2):N1}";
+                DownloadText.Text = $"UL: {text}";
+            }
+            return text;
+        }
+        private Icon getUploadIcon()
+        {
+            using (Bitmap bitmap = new Bitmap(32, 32))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    Font font = new Font("Microsoft Sans Serif", 20, FontStyle.Regular, GraphicsUnit.Pixel);
+                    string iconText = getUploadSpeed();
+                    g.DrawString(iconText, font, new SolidBrush(Color.Red), new PointF(-2, -2));
+                }
+                return Icon.FromHandle(bitmap.GetHicon());
+            }
+        }
+        private Icon getDownloadIcon()
+        {
+            using (Bitmap bitmap = new Bitmap(32, 32))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    Font font = new Font("Microsoft Sans Serif", 20, FontStyle.Regular, GraphicsUnit.Pixel);
+                    string iconText = getDownloadSpeed();
+                    g.DrawString(iconText, font, new SolidBrush(Color.Green), new PointF(-2, -2));
+                }
+                return Icon.FromHandle(bitmap.GetHicon());
+            }
+        }
+        private void Task()
+        {
+            try
+            {
+                Download.Icon = getDownloadIcon();
+                Upload.Icon = getUploadIcon();
+            }
+            catch (Exception ex)
+            {
+                //continue;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+            Timer timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 500;
+            timer.Tick += (s, ex) => Task();
+            networkMonitor.StartMonitoring();
+            Download.Visible = true;
+            Upload.Visible = true;
+        }
+
+        private void TrayMenuContext()
+        {
+            this.Download.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.Download.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit_Click);
+
+            this.Upload.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.Upload.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit_Click);
+        }
+
+        void MenuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Download.Visible = false;
+            Upload.Visible = false;
+        }
+    }
+}
